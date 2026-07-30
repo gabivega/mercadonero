62bfc4fa3d81fb6663e38dad9ca26d419f3ad31f
Gabi <gabi.vega1988@gmail.com>
2026-07-30 15:34:03 -0300

    30-07-26

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -94,7 +94,7 @@ export default function App() {
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
-        <DisclaimerBanner />
+        {/* <DisclaimerBanner /> */}
         <Footer />
       </div>
     </BrowserRouter>
